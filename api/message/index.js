6baa6e10a53482d4e6541c015b365c8a17c67655
jumbli <brett@headstartdesign.co.uk>
2022-10-23 18:18:23 +0100

    Added login to diagnose server issues

--- api/message/index.js
+++ api/message/index.js
@@ -1,24 +1,33 @@
 module.exports = async function (context, req)
 {
-
-    var token = await GetToken();
-    var response = await AnalyseDocument(token,req.body.document);
+    context.log.info("running message");
+    var token = await GetToken(context);
+    context.log.info("got token");
+    var response = await AnalyseDocument(context,token,req.body.document);
+    context.log.info("got document");
     
-    if (typeof(response) ==  "string") // This happens if an error message is returned
+    if (typeof(response) ==  "string") { // This happens if an error message is returned
         response = JSON.stringify(response);
-
+        context.log.error("Error returned:" + response);
+    }
+    context.log.info(response);
     console.log(response);
     context.res.body = response; // Needs to be a valid JSON object or an error message string
 
 };
 
-const GetToken = async () =>
+const GetToken = async (context) =>
 {
+    context.log.info("in getToken");
     var response = '';
     const axios = require('axios');
+    context.log.info("loaded axios");
     
+    
+    context.log.info("set username is" + process.env["expertAiUsername"]);
     // POST request using axios with set headers
     const inputs = {username:  process.env["expertAiUsername"], password: process.env["expertAiPassword"]};
+    context.log.info("set inputs");
     const config = { 
         headers: {
             'Accept': 'application/json',
@@ -26,18 +35,21 @@ const GetToken = async () =>
         },
         forcedJSONParsing: false
     };
+    context.log.info("set headers");
     try {
         response = await axios.post('https://developer.expert.ai/oauth2/token', inputs, { config } );
+        context.log.info("got response");
         response = response.data;
     } catch (err)
     {
+        context.log.info("got error:" + err);
         response = "ERROR";
     }
     return response;
     
 };
 
-const AnalyseDocument = async (token, doc) =>
+const AnalyseDocument = async (context, token, doc) =>
 {
     const axios = require('axios');
 
